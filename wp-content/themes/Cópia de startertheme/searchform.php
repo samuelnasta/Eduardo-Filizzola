d90e82df935e7ff05bbeb16c2fbab9060070e095
samuelnasta <sarcastikpl@gmail.com>
2012-07-19 10:51:52 -0300

    Versão final

--- wp-content/themes/Cópia de startertheme/searchform.php
+++
@@ -1,4 +0,0 @@
-<form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
-	<input type="text" value="Search" onclick="value=''" name="s" id="s" />
-	<input type="submit" id="searchsubmit" value="Search" />
-</form>
